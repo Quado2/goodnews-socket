5217247fab491ddf63577674b3e275753187f0c0
Quado2 <engchikwas404@gmail.com>
2022-03-21 16:23:25 +0100

    First push

---
+++ sockets.js
@@ -0,0 +1,30 @@
+const app = require("express")();
+const server = require("http").createServer(app);
+const cors = require("cors");
+
+const io = require("socket.io")(server, {
+	cors: {
+		origin: "*",
+		methods: [ "GET", "POST" ]
+	}
+});
+
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+	res.send('Running');
+});
+
+io.on("connection", (socket) => {
+	socket.emit("me", socket.id);
+
+	socket.on("disconnect", () => {
+		socket.broadcast.emit("callEnded")
+	});
+
+
+});
+
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
