a927a43a2f284c2b7bda4b8c0a14b9f5b56e0d46
Quado2 <engchikwas404@gmail.com>
2022-03-21 16:27:03 +0100

    continued on it

--- index.js
+++ index.js
@@ -18,19 +18,15 @@ app.get('/', (req, res) => {
 });
 
 io.on("connection", (socket) => {
+	console.log("We are connected")
+	
 	socket.emit("me", socket.id);
 
 	socket.on("disconnect", () => {
-		socket.broadcast.emit("callEnded")
+		
 	});
 
-	socket.on("callUser", ({ userToCall, signalData, from, name }) => {
-		io.to(userToCall).emit("callUser", { signal: signalData, from, name });
-	});
 
-	socket.on("answerCall", (data) => {
-		io.to(data.to).emit("callAccepted", data.signal)
-	});
 });
 
 server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
